50e9477262b329f188ece13eefeaa7d6a4b796fb
Matt Jackson <mattjackson@matts-mbp.mynet>
2020-05-04 14:16:54 +0100

    initial commit

---
+++ firstJS.js
@@ -0,0 +1,40 @@
+let afcb = ((10 * 2) + 1) / 5;
+let cheese = 5;
+
+
+let ten = 10;
+console.log(ten * ten);
+// → 100
+
+
+let x = 30;
+console.log("the value of x is", x);
+
+console.log(Math.max(2, 4));
+
+let theNumber = Number(prompt("Pick a number"));
+if (!Number.isNaN(theNumber)) {
+  console.log("Your number is the square root of " +
+              theNumber * theNumber);
+}
+
+console.group
+(function(){
+  "use strict";
+  /* Start of your code */
+  function greetMe(yourName) {
+    alert('Hello ' + yourName);
+  }
+
+  greetMe('World');
+  /* End of your code */
+})();
+
+// xcvbccgndfghfghfhgfh
+// 
+
+/*
+
+yououoouououo
+*/
+
---
+++ secondJS.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+alert( admin );
+
+prompt("How are you today?", "" )
+
+alert( Number('nine') );  
